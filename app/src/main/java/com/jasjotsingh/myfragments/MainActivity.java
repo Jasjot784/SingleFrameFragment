2041687637d76c7ee94d9a93d733db6a200963de
Jasjot Singh <jasjot784@gmail.com>
2019-06-03 12:22:19 +0530

    SingleFrameFragment

---
+++ app/src/main/java/com/jasjotsingh/myfragments/MainActivity.java
@@ -0,0 +1,43 @@
+package com.jasjotsingh.myfragments;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Configuration config = getResources().getConfiguration();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        /**
+         * Check the device orientation and act accordingly
+         */
+
+        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            /**
+             * Landscape mode of the device
+             */
+            LM_Fragment ls_fragment = new LM_Fragment();
+            fragmentTransaction.replace(android.R.id.content, ls_fragment);
+        } else {
+            /**
+             * Portrait mode of the device
+             */
+            PM_Fragment pm_fragment = new PM_Fragment();
+            fragmentTransaction.replace(android.R.id.content, pm_fragment);
+        }
+        fragmentTransaction.commit();
+    }
+
+}
+
+
